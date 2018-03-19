09deb04c44f22c38f0be16c7ad39a23ba75c67f1
Jasper Tamminga <jasper.tamminga@qnh.nl>
2018-03-19 13:27:47 +0100

    Display identity balance

--- src/app/identity/identity.component.ts
+++ src/app/identity/identity.component.ts
@@ -1,3 +1,4 @@
+import { Web3Service } from './../web3.service';
 import { Identity } from './../vault/identity';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -10,14 +11,17 @@ import { VaultService } from './../vault/vault.service';
 })
 export class IdentityComponent implements OnInit {
   identity: Identity;
+  balance: 0;
 
   constructor(
     private route: ActivatedRoute,
-    private vault: VaultService
+    private vault: VaultService,
+    private web3Service: Web3Service
   ) { }
 
   ngOnInit() {
     this.identity = this.vault.getIdentity(this.route.snapshot.paramMap.get('address'));
+    this.web3Service.web3.eth.getBalance(this.identity.address).then((balance) => { this.balance = balance; });
   }
 
 }
