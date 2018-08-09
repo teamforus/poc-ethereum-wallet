271f1f4fb061fb89a51b0a66e209dd796d96564e
Jasper Tamminga <jasper.tamminga@qnh.nl>
2018-08-09 14:17:55 +0200

    Refresh Currencies page on show

--- src/app/currencies/currencies.component.ts
+++ src/app/currencies/currencies.component.ts
@@ -32,61 +32,66 @@ export class CurrenciesComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    const keys = this.vault.getKeys();
-    for (const key of keys) {
-      const account = this.web3Service.web3.eth.accounts.privateKeyToAccount(key.key);
-      this.ethBalances.push(
-        {
-          address: this.web3Service.web3.eth.accounts.privateKeyToAccount(key.key).address,
-          balance: this.web3Service.web3.eth.getBalance(account.address),
-          type: BalanceType.Key
-        }
-      );
-    }
-
-    const identities = this.vault.getIdentities();
-    for (const identity of identities) {
-      this.ethBalances.push(
-        {
-          address: identity.address,
-          balance: this.web3Service.web3.eth.getBalance(identity.address),
-          type: BalanceType.Identity
-        }
-      );
-    }
-
-    const tokenAddresses = this.vault.getTokens();
-    for (const tokenAddress of tokenAddresses) {
-      const contract = new this.web3Service.web3.eth.Contract(Erc20ContractData.abi, tokenAddress);
-      const token = {
-        address: tokenAddress,
-        name: contract.methods.name().call(),
-        symbol: '',
-        balances: new Array<Balance>()
-      };
-      for (const key of keys) {
-        token.balances.push({
-          address: key.address,
-          balance: contract.methods.balanceOf(key.address).call(),
-          type: BalanceType.Key
-        });
-      }
-      for (const identity of identities) {
-        token.balances.push({
-          address: identity.address,
-          balance: contract.methods.balanceOf(identity.address).call(),
-          type: BalanceType.Identity
-        });
-      }
-
-      this.tokens.push(token);
-    }
 
   }
 
   @HostListener('window:show', ['$event'])
   onShow(event) {
-    //console.log(event);
+    if ('currencies' === event.target.id) {
+
+      this.ethBalances.length = 0;
+      this.tokens.length = 0;
+
+      const keys = this.vault.getKeys();
+      for (const key of keys) {
+        const account = this.web3Service.web3.eth.accounts.privateKeyToAccount(key.key);
+        this.ethBalances.push(
+          {
+            address: this.web3Service.web3.eth.accounts.privateKeyToAccount(key.key).address,
+            balance: this.web3Service.web3.eth.getBalance(account.address),
+            type: BalanceType.Key
+          }
+        );
+      }
+
+      const identities = this.vault.getIdentities();
+      for (const identity of identities) {
+        this.ethBalances.push(
+          {
+            address: identity.address,
+            balance: this.web3Service.web3.eth.getBalance(identity.address),
+            type: BalanceType.Identity
+          }
+        );
+      }
+
+      const tokenAddresses = this.vault.getTokens();
+      for (const tokenAddress of tokenAddresses) {
+        const contract = new this.web3Service.web3.eth.Contract(Erc20ContractData.abi, tokenAddress);
+        const token = {
+          address: tokenAddress,
+          name: contract.methods.name().call(),
+          symbol: '',
+          balances: new Array<Balance>()
+        };
+        for (const key of keys) {
+          token.balances.push({
+            address: key.address,
+            balance: contract.methods.balanceOf(key.address).call(),
+            type: BalanceType.Key
+          });
+        }
+        for (const identity of identities) {
+          token.balances.push({
+            address: identity.address,
+            balance: contract.methods.balanceOf(identity.address).call(),
+            type: BalanceType.Identity
+          });
+        }
+
+        this.tokens.push(token);
+      }
+    }
   }
 
   transferFromKey(address) {
