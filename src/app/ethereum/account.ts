973ed430c423c6776460b3c3cb79382b5a811f46
Jasper Tamminga <jasper.tamminga@qnh.nl>
2018-09-11 08:38:10 +0200

    Added ether balance to Account

--- src/app/ethereum/account.ts
+++ src/app/ethereum/account.ts
@@ -1,18 +1,27 @@
 import { Keyring } from './keyring';
-import { Address, Signature, PrivateKey, SignData } from './types';
+import { Address, Signature, PrivateKey, SignData, EtherBalance } from './types';
 import { Transaction } from './types';
 
 export class Account {
+
+  balance: EtherBalance = 0;
 
   constructor(
     private address: Address,
     // @ts-ignore
     private web3: Web3,
     private keyring: Keyring
-  ) {}
+  ) {
+    this.updateBalance();
+    this.web3.eth.subscribe('newBlockHeaders', this.updateBalance );
+  }
 
   valueOf() {
     return this.getAddress();
+  }
+
+  private updateBalance() {
+    this.web3.eth.getBalance(this.address).then(balance => { this.balance = balance; });
   }
 
   private getPrivateKey(): Promise<PrivateKey> {
@@ -30,4 +39,5 @@ export class Account {
   async signTransaction(trx: Transaction): Promise<Object> {
     return this.web3.signTransaction(trx, await this.getPrivateKey());
   }
+
 }
--- src/app/ethereum/types.ts
+++ src/app/ethereum/types.ts
@@ -2,6 +2,7 @@ export type Address = string;
 export type Signature = string;
 export type SignData = string;
 export type PrivateKey = string;
+export type EtherBalance = number;
 export interface Keypair {public: string; private: string; }
 export interface Transaction {
   from?: Address;
